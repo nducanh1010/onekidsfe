09addd420c1186edb771b5a8bd12aa93369a51de
Duc Anh <nducanh1010@gmail.com>
2023-06-12 15:19:58 +0700

    remove old rter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router/index2";
+import router from "./router";
 import Vuex from "vuex";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
